557e2520233fe6fabc5179ccc9597f6d9b42e50b
talitast <talitatravassos.tt@gmail.com>
2019-04-16 09:21:47 -0300

    added UserInput component

---
+++ src/components/UserInput.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const initialState = '# Sample Markdown Heading\n\nEdit or replace this\n-----------\n\n### Another deeper heading\n\nParagraphs are separated by a blank line.\n\nLeave 2 spaces at the end of a line to do a  line break\n\nText attributes *italic*, **bold**,\n`monospace`, ~~strikethrough~~ .\n\nUnordered list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\n---\n\n#### Created by:\n[Jay Karlsven](https://jpkarlsven.com \"Jay Karlsven\'s Website\")'
+
+const UserInput = (props) => {
+
+    const [md, setMd] = useState(initialState);
+
+    const updateMd = (e) => {
+        setMd(e.target.value)
+    }
+
+    return (
+        <div className="row">
+            <div className="col-md-6">
+                <h3 className="text-center">Enter the Markdown</h3>
+                <textarea 
+                    type="text" 
+                    className="md-input" 
+                    value={md} 
+                    onChange={updateMd} />
+
+            </div>
+            <div className="col-md-6">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default UserInput
